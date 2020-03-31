284dba593ffcade0e93b064877174672a3377c60
muink <hukk1996@gmail.com>
2020-03-31 22:11:33 +0800

    fix MKKEY

--- MakeInit.cmd
+++ MakeInit.cmd
@@ -66,14 +66,11 @@ md "%CURRENTPC%\%CURRENTUSER%\AppData\Roaming\Microsoft\Windows" 2>nul || goto :
 pushd "%CURRENTPC%\%CURRENTUSER%\AppData\Roaming\Microsoft\Windows"
 call:[WTini] "%CD%" "" 69
 setlocal enabledelayedexpansion
-set "PRESET=Network Shortcuts=imageres.dll,28:Printer Shortcuts=imageres.dll,48:SendTo=imageres.dll,176:Themes=themeui.dll,0"
+set "KEY=Network Shortcuts=imageres.dll:28;Printer Shortcuts=imageres.dll:48;SendTo=imageres.dll:176;Themes=themeui.dll:0"
 :--windows--#loop
-for /f "tokens=1* delims=:" %%i in ("!PRESET!") do (
-	for /f "tokens=1* delims==" %%n in ("%%~i") do (
-		md "%%~n" 2>nul
-		for /f "tokens=1-2 delims=," %%p in ("%%~o") do call:[WTini] "%CD%\%%~n" %%~p %%~q
-	)
-	set "PRESET=%%j"
+for /f "tokens=1* delims=;" %%i in ("!KEY!") do (
+	for /f "tokens=1,2 delims==" %%k in ("%%i") do call:[MKKEY] "%CD%" "%%~k" "%%~l"
+	set "KEY=%%j"
 	goto :--windows--#loop
 )
 endlocal
@@ -98,5 +95,17 @@ set "pa=%~1"
 echo.IconResource=%SystemRoot%\system32\%icolib%,%~3)>"%pa%\desktop.ini"
 attrib +r "%pa%"
 attrib +s +h "%pa%\desktop.ini"
+endlocal
+goto :eof
+
+:[MKKEY]
+setlocal enabledelayedexpansion
+set "pa=%~1\%~2"
+set "key=%~2"
+set "value=%~3"
+set "value2=%value::=" "%"
+if "%value%" == "%value2%" set value2=" "%value%
+md "%pa%" 2>nul
+call:[WTini] "%pa%" "%value2%"
 endlocal
 goto :eof
